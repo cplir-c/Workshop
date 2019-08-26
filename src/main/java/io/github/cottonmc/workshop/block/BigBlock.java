eda901cccbdaaa7316d5e783aa15bb8557ad51c7
Meredith Espinosa <goclonefilms@gmail.com>
2019-08-25 22:48:54 -0700

    keep sketching BigBlock

--- src/main/java/io/github/cottonmc/workshop/block/BigBlock.java
+++ src/main/java/io/github/cottonmc/workshop/block/BigBlock.java
@@ -2,11 +2,14 @@ package io.github.cottonmc.workshop.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.ViewableWorld;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public abstract class BigBlock extends Block {
 
@@ -14,28 +17,40 @@ public abstract class BigBlock extends Block {
 		super(settings);
 	}
 
-	//TODO: support for more blocker locations
 	public abstract Vec3i[] getBlockerOffsets(Direction facing);
+
+	@Nullable
+	abstract EnumProperty<Direction> getFacingProperty();
 
 	public abstract BlockerBlock getBlocker();
 
 	@Override
 	public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState newState, boolean boolean_1) {
-		//TODO: support for more blocker locations
 		super.onBlockAdded(state, world, pos, newState, boolean_1);
-		world.setBlockState(pos.up(), WorkshopBlocks.BLOCKER.getDefaultState());
+		Direction dir = getFacingProperty() == null? Direction.NORTH : state.get(getFacingProperty());
+		for (Vec3i offset : getBlockerOffsets(dir)) {
+			BlockPos toAdd = pos.add(offset);
+			world.setBlockState(toAdd, getBlocker().getDefaultState());
+		}
 	}
 
 	@Override
 	public void onBlockRemoved(BlockState state, World world, BlockPos pos, BlockState newState, boolean boolean_1) {
-		//TODO: support for more blocker locations
 		super.onBlockRemoved(state, world, pos, newState, boolean_1);
-		world.breakBlock(pos.up(), false);
+		Direction dir = getFacingProperty() == null? Direction.NORTH : state.get(getFacingProperty());
+		for (Vec3i offset : getBlockerOffsets(dir)) {
+			BlockPos toRemove = pos.add(offset);
+			world.breakBlock(toRemove, false);
+		}
 	}
 
 	@Override
 	public boolean canPlaceAt(BlockState state, ViewableWorld world, BlockPos pos) {
-		//TODO: support for more blocker locations
-		return world.getBlockState(pos.up()).getMaterial().isReplaceable();
+		Direction dir = getFacingProperty() == null? Direction.NORTH : state.get(getFacingProperty());
+		for (Vec3i offset : getBlockerOffsets(dir)) {
+			BlockPos blocker = pos.add(offset);
+			if (!world.getBlockState(blocker).getMaterial().isReplaceable()) return false;
+		}
+		return true;
 	}
 }
