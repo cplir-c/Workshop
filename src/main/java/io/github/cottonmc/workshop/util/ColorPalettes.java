891b4d1301ca4e8b65bbb558d655b2c3f3b0b155
Meredith Espinosa <goclonefilms@gmail.com>
2019-08-26 23:21:39 -0700

    add some more color palette codes

--- src/main/java/io/github/cottonmc/workshop/util/ColorPalettes.java
+++ src/main/java/io/github/cottonmc/workshop/util/ColorPalettes.java
@@ -1,10 +1,13 @@
 package io.github.cottonmc.workshop.util;
 
 public class ColorPalettes {
-	//wood colors
-	public static final int VANILLA = 0x493608;
+	//vanilla material colors
+	public static final int WOOD = 0x493608;
+	public static final int STONE = 0x565656;
 
 	//bindings
+	public static final int GRASS_ROPE = 0x2b7b1a;
+	public static final int VINE_ROPE = 0x203900;
 
 	//metals
 	public static final int IRON = 0xc2c2c2;
