642f6ea992d21caf11c22cfe38fb5b1a4ada19dc
cplir-c <31234188+cplir-c@users.noreply.github.com>
2019-09-07 13:05:01 -0500

    implement some of ToolFurnaceRecipe

--- src/main/java/io/github/cottonmc/workshop/recipe/ToolFurnaceRecipe.java
+++ src/main/java/io/github/cottonmc/workshop/recipe/ToolFurnaceRecipe.java
@@ -1,39 +1,57 @@
 package io.github.cottonmc.workshop.recipe;
 
 import io.github.cottonmc.workshop.block.entity.ToolFurnaceBlockEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.RecipeType;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.PacketByteBuf;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 
-public class ToolFurnaceRecipe implements Recipe<ToolFurnaceBlockEntity> {
-	//TODO: implement
+import static io.github.cottonmc.workshop.recipe.WorkshopRecipes.TOOL_FURNACE_RECIPE_SERIALIZER;
 
+import com.google.gson.JsonObject;
+
+import io.github.cottonmc.workshop.Workshop;
+
+public class ToolFurnaceRecipe implements Recipe<ToolFurnaceBlockEntity> {
+	
+	ItemStack input;
+	ItemStack mold;
+	ItemStack output;
+	
+	ToolFurnaceRecipe(ItemStack input, ItemStack mold, ItemStack output){
+		this.input = input;
+		this.mold = mold;
+		this.output = output;
+	}
+	
 	@Override
 	public boolean matches(ToolFurnaceBlockEntity be, World world) {
-		return false;
+		return be.getInvStack(ToolFurnaceBlockEntity.INPUT).equals(input) && be.getInvStack(ToolFurnaceBlockEntity.MOLD).equals(mold);
 	}
 
 	@Override
 	public ItemStack craft(ToolFurnaceBlockEntity be) {
-		return null;
+		return output;
 	}
 
 	@Override
-	public boolean fits(int i, int i1) {
-		return false;
+	public boolean fits(int craftingWidth, int craftingHeight) {
+		return craftingWidth * craftingHeight <= 2 && craftingWidth != 0 && craftingHeight != 0;
 	}
 
 	@Override
 	public ItemStack getOutput() {
-		return null;
+		return output;
 	}
 
 	@Override
 	public Identifier getId() {
-		return null;
+		return new Identifier(Workshop.MODID, "tool_furnace_recipe_" + Item.getRawId(input.getItem()));
 	}
 
 	@Override
